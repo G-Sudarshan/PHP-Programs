530b7f8e5154926a0f90d250d7617065f29f46c4
SHAU06 <52189787+SHAU06@users.noreply.github.com>
2020-10-01 13:22:01 +0530

    Code optimized and one condition added.
    To get file extentions, strtolower(), end() and explode functions used together to optimized lines of code and variable declarations.
    For more accuraccy condition checked at move_uploaded_file() function.

--- File Uploading/index.php
+++ File Uploading/index.php
@@ -21,26 +21,30 @@
 			$file_temp = $_FILES['image']['tmp_name'];
 			$file_type = $_FILES['image']['type'];
 
-			$tmp = explode('.', $_FILES['image']['name']);
-			$file_ext = end($tmp);
+			$file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));	// File name will get converted to lowercase and remaining code will return file extension & also this will reduce code..		
 			$extension = array("jpeg","jpg","png");
 			$error = array();
 			if(in_array($file_ext, $extension)=== false)
 			{
-				$error[] = "Extension not allowed please enter valid extension";
+				$error[] = "Extension not allowed please enter valid extension!!!";
 			}
 			if($file_size > 2097152)
 			{
-				$error[] = "file size must be exactly 2 MB";
+				$error[] = "file size must be up to 2 MB";
 			}
 			if(empty($error)== true)
 			{
-				move_uploaded_file($file_temp, "image/.".$file_name);
-				echo "<h5 class='text-success'><br>File Successfully Uploades</h5><br>";
-				echo "<h5 class='text-primary'>File Name : $file_name</h5>";
-				echo "<h5 class='text-primary'>File Extentension : $file_ext</h5>";
-				echo "<h5 class='text-primary'>File Type : $file_type</h5>";
-				echo "<h5 class='text-primary'>File Size : $file_size KB</h5>";
+				if(move_uploaded_file($file_temp, "image/.".$file_name))	// Checked for file uploaded or not
+				{
+					echo "<h5 class='text-success'><br>File Successfully Uploaded</h5><br>";
+					echo "<h5 class='text-primary'>File Name : $file_name</h5>";
+					echo "<h5 class='text-primary'>File Extentension : $file_ext</h5>";
+					echo "<h5 class='text-primary'>File Type : $file_type</h5>";
+					echo "<h5 class='text-primary'>File Size : $file_size KB</h5>";
+				}
+				else{
+					echo "<h5 class='text-danger'><br>Something went wrong..! File not uploaded..!!</h5><br>";
+				}
 			}
 			else 
 			{
